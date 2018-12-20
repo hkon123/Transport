f964a112033c567684eef7b68156eb35c5260461
hkon123 <hkonhd@gmail.com>
2018-12-20 21:49:59 +0000

    update 0.1
    added functionality
    -removing cars whenthey reach end of bridge
    -allowing cars to speed up and slow down depending on distance to the car infront
    -running the cars across the bridge atumaticly
    -adding cars at a defined interval

--- road.py
+++ road.py
@@ -6,9 +6,10 @@ from car import Car
 class Road(object):
 
     def __init__(self,timeStep):
-        self.cars = np.zeros(10, dtype = object)
+        self.cars = np.zeros(100, dtype = object)
         self.timeStep = timeStep
         self.carsOnRoad = 0
+        self.roadLength = 2512
 
     def addCar(self):
         self.cars[self.carsOnRoad] = Car()
@@ -19,10 +20,23 @@ class Road(object):
         self.carsOnRoad -=1
 
     def updatePositions(self):
+        index = 0
         for car in self.cars:
             if isinstance(car,int)!=True:
+                while True:
+                    if self.nearestCar(car)>3:
+                        while car.currentSpeed<car.preferedSpeed:
+                            car.currentSpeed +=0.1
+                    else:
+                        while self.nearestCar(car)<3:
+                            car.currentSpeed -=0.1
+                    break
                 car.previousPosition = car.position
                 car.position = car.position + car.currentSpeed*self.timeStep
+                if car.position >=self.roadLength:
+                    self.removeCar(index)
+                index+=1
+
 
     def nearestCar(self, currentCar):
         minDistance = 1000
@@ -35,15 +49,30 @@ class Road(object):
         return float(minDistance)/currentCar.currentSpeed
 
 
+    def drive(self,addCarIntervall):
+        currentTime = 0
+        while currentTime<100:
+            if currentTime%addCarIntervall == 0 and currentTime!=0:
+                self.addCar()
+            self.printSpeeds()
+            self.updatePositions()
+            currentTime+=self.timeStep
+
+
+    def printPositions(self):
+        for car in self.cars:
+            if isinstance(car,int)!=True:
+                print(car.position)
+        print("\n \n")
+
+    def printSpeeds(self):
+        for car in self.cars:
+            if isinstance(car,int)!=True:
+                print(car.currentSpeed)
+        print("\n \n")
+
 
 A=Road(1)
 A.addCar()
 
-print(A.cars[0].position)
-A.updatePositions()
-A.addCar()
-print(A.cars[0].position)
-A.updatePositions()
-print(A.cars[0].position)
-print(A.cars[1].position)
-print(A.nearestCar(A.cars[1]))
+A.drive(2)
